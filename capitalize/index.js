a275f9cc3274b6a5e537fd8eb728abcbb2595b8d
Traian Rusu <traianrusu1@gmail.com>
2019-02-11 19:24:57 -0500

    added a few more algorithms and tests for fizzbuzz and capitalize

---
+++ capitalize/index.js
@@ -0,0 +1,18 @@
+// A function that accepts a string and should
+// capitalize the first letter of each word in the string then
+// return the capitalized string.
+// --- Examples
+//   capitalize('a short sentence') --> 'A Short Sentence'
+//   capitalize('a lazy fox') --> 'A Lazy Fox'
+//   capitalize('look, it is working!') --> 'Look, It Is Working!'
+
+function capitalize(str) {
+  return str
+    .split(" ")
+    .map(val => {
+      return val[0].toUpperCase() + val.slice(1);
+    })
+    .join(" ");
+}
+
+module.exports = capitalize;
